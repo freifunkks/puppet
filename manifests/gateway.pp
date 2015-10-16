536ff15f50ca2e438867868f0c9261585771cdba
Philipp Hirsch <itself@hanspolo.net>
2015-10-16 21:55:12 +0200

    Load fastd secret_key from file and use systemd for services

--- manifests/gateway.pp
+++ manifests/gateway.pp
@@ -1,5 +1,9 @@
 class base_node() {
 
+  Service {
+    provider => systemd,
+  }
+
   class { 'apt': }
 
   # update packages before we install any
@@ -43,6 +47,6 @@ node 'vpn2' {
   class { 'vpn':
     ip_addr => '37.120.176.206',
     vpn_nr  => '2',
-    secret_key => '123'
+    secret_key => file('/root/fastd_secret_key')
   }
 }
